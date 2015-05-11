2d10617113d69d8a56b1ac9e6e2c7cf9e8643028
Roger Rush <rjrush789@gmail.com>
2015-05-11 16:07:21 -0700

    Change names to reflect behavior
    For Reference's internal data structures

--- reference.rb
+++ reference.rb
@@ -5,21 +5,21 @@ require_relative 'characters'
 
 class Reference
   include Singleton
-  attr_reader :hash, :trie
+  attr_reader :codepoint_to_data, :name_to_data
 
   def initialize(data_source=UnicodeData.instance)
     # Master storage object
-    # - referenced by internal data structures (hash, trie)
+    # - referenced by internal data structures (codepoint_to_data, name_to_data)
 
-    @hash = data_source.hash
-    @trie = data_source.trie
+    @codepoint_to_data = data_source.hash
+    @name_to_data = data_source.trie
   end
 
   def name(codepoint)
     # -> String
     # Map a codepoint to the corresponding name.
 
-    val = @hash[codepoint]
+    val = @codepoint_to_data[codepoint]
     val.correction.empty? ? val.name : val.correction
   end
 
@@ -27,20 +27,20 @@ class Reference
     # -> Codepoint
     # Map a string representing a name, or an alias, to a codepoint.
 
-    @trie[name]
+    @name_to_data[name]
   end
 
   def majorCategory(codepoint)
     # -> String
     # Map a codepoint to a string of length 1-3 that represents its major category.
 
-    @hash[codepoint].major_category
+    @codepoint_to_data[codepoint].major_category
   end
 
   def category(codepoint)
     # -> String
     # Map a codepoint to a string of length 2 that represents its category.
 
-    @hash[codepoint].minor_category
+    @codepoint_to_data[codepoint].minor_category
   end
 end
